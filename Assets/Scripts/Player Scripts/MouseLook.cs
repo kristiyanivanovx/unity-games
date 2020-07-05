d759a35a3b6544351ac0c701c7279eea7676f229
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-07-05 12:03:19 +0300

    upload files

---
+++ Assets/Scripts/Player Scripts/MouseLook.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	[SerializeField]
+	private Transform playerRoot, lookRoot;
+
+	[SerializeField]
+	private bool invert;
+
+	[SerializeField]
+	private bool can_Unlock = true;
+
+	[SerializeField]
+	private float sensetivity = 5f;
+
+	[SerializeField]
+	private int smooth_Steps = 10;
+
+	[SerializeField]
+	private float smooth_weight = 0.4f;
+
+	[SerializeField]
+	private float roll_Angle = 0f;
+
+	[SerializeField]
+	private float roll_Speed = 3f;
+
+	[SerializeField]
+	private Vector2 default_Look_Limits = new Vector2(-90f, 90f);
+
+	private Vector2 look_Angles;
+	private Vector2 current_Mouse_Look;
+	private Vector2 smooth_move;
+
+	private float current_Roll_Angle;
+
+	private int last_Look_Frame;
+
+	// Use this for initialization
+	void Start () {
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		LockAndUnlockCursor();
+
+		if(Cursor.lockState == CursorLockMode.Locked) {
+			LookAround();
+		}
+	}
+	
+	// Lock and Unlock
+	void LockAndUnlockCursor() {
+
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+
+			if (Cursor.lockState == CursorLockMode.Locked) {
+				Cursor.lockState = CursorLockMode.None;
+			}
+			else {
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+		}
+	}
+
+	void LookAround() {
+		
+		current_Mouse_Look = new Vector2(Input.GetAxis(MouseAxis.MOUSE_Y),  // MOUSE_Y is for left and right,
+										 Input.GetAxis(MouseAxis.MOUSE_X)); // MOUSE_X is for up and down
+
+		// Still, we are assigning MOUSE_Y to look_Angles.x and
+		//                         MOUSE_X to look_Angles.y
+		look_Angles.x += current_Mouse_Look.x * sensetivity * (invert ? 1f : -1f); // Left and right
+		look_Angles.y += current_Mouse_Look.y * sensetivity;					   // Up and Down
+
+		// Clamp doesnt allow look_angles.x to go lower than default_Look_Limits.x and above  default_Look_Limits.y
+		look_Angles.x = Mathf.Clamp(look_Angles.x, default_Look_Limits.x, default_Look_Limits.y);
+
+
+		// current_Roll_Angle = Mathf.Lerp(current_Roll_Angle,					    // go from this value
+		// Input.GetAxisRaw(MouseAxis.MOUSE_X) * roll_Angle,                    	// to this value
+		// Time.deltaTime * roll_Speed);                                            // in this time
+
+		lookRoot.localRotation = Quaternion.Euler(look_Angles.x, 0f, 0f);
+		playerRoot.localRotation = Quaternion.Euler(0f, look_Angles.y, 0f);
+
+	}
+}
+
---
+++ Assets/Scripts/Player Scripts/PlayerSprintAndCrouch.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprintAndCrouch : MonoBehaviour {
+
+	private PlayerMovement playerMovement;
+
+	public float sprint_Speed = 10f;
+	public float move_Speed = 5f;
+	public float crouch_Speed = 1.5f;
+
+	private Transform look_Root;
+	private float stand_Height = 1.6f;
+	private float crouch_Height = 1f;
+
+	private bool is_Crouching;
+
+	private PlayerFootsteps player_Footsteps;
+
+	private float sprint_Volume = 1f;
+	private float crouch_Volume = 0.1f;
+	private float walk_Volume_Min = 0.2f, walk_Volume_Max = 0.6f;
+
+	private float walk_Step_Distance = 0.4f;
+	private float sprint_Step_Distance = 0.25f;
+	private float crouch_Step_Distance = 0.5f;
+
+	private PlayerStats player_Stats;
+
+	private float sprint_Value = 100f;
+	public float sprint_Treshold = 15f;
+
+
+	void Awake () {
+
+		playerMovement = GetComponent<PlayerMovement>();
+
+		look_Root = transform.GetChild(0);
+
+		player_Footsteps = GetComponentInChildren<PlayerFootsteps>();
+
+		player_Stats = GetComponent<PlayerStats>();
+
+	}
+
+	void Start() {
+		player_Footsteps.volume_Min = walk_Volume_Min;
+		player_Footsteps.volume_Max = walk_Volume_Max;
+		player_Footsteps.step_Distance = walk_Step_Distance;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Sprint();
+		Crouch();
+	}
+
+	// Sprint
+	void Sprint() {
+
+		// if we have stamina, we can sprint
+		if (sprint_Value > 0f) {
+
+			if (!is_Crouching && Input.GetKeyDown(KeyCode.LeftShift) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))) {
+
+
+				playerMovement.speed = sprint_Speed;
+
+				player_Footsteps.step_Distance = sprint_Step_Distance;
+
+				player_Footsteps.volume_Min = sprint_Volume;
+
+				player_Footsteps.volume_Max = sprint_Volume;
+
+			}
+
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching) {
+
+			playerMovement.speed = move_Speed;
+
+			player_Footsteps.step_Distance = walk_Step_Distance;
+			player_Footsteps.volume_Min = walk_Volume_Min;
+			player_Footsteps.volume_Max = walk_Volume_Max;
+			
+		}
+
+		if (Input.GetKey(KeyCode.LeftShift) && !is_Crouching) {
+
+			sprint_Value -= sprint_Treshold * Time.deltaTime;
+
+			if (sprint_Value <= 0f) {
+
+				sprint_Value = 0f;
+
+				// reset the speed and sound
+				playerMovement.speed = move_Speed;
+				player_Footsteps.step_Distance = walk_Step_Distance;
+				player_Footsteps.volume_Min = walk_Volume_Min;
+				player_Footsteps.volume_Max = walk_Volume_Max;
+
+			}
+
+			player_Stats.Display_StaminaStats(sprint_Value);
+
+		}
+		else {
+
+			if (sprint_Value != 100) {
+
+				sprint_Value += (sprint_Treshold / 2f) * Time.deltaTime;
+
+				player_Stats.Display_StaminaStats(sprint_Value);
+
+				if (sprint_Value > 100f)
+					sprint_Value = 100f;
+
+			}
+
+		}
+	}
+
+	// Crouch
+	void Crouch() {
+
+		if (Input.GetKeyDown(KeyCode.LeftControl)){
+
+			if (is_Crouching) {
+
+				look_Root.localPosition = new Vector3(0F, stand_Height, 0f);
+				playerMovement.speed = move_Speed;
+
+				player_Footsteps.step_Distance = walk_Step_Distance;
+				player_Footsteps.volume_Min = walk_Volume_Min;
+				player_Footsteps.volume_Max = walk_Volume_Max;
+
+				is_Crouching = false;
+
+			}
+			else {
+
+				look_Root.localPosition = new Vector3(0F, crouch_Height, 0f);
+				playerMovement.speed = crouch_Speed;
+
+				player_Footsteps.step_Distance = crouch_Step_Distance;
+				player_Footsteps.volume_Min = crouch_Volume;
+				player_Footsteps.volume_Max = crouch_Volume;
+
+				is_Crouching = true;
+
+			}
+		}
+	}
+}
